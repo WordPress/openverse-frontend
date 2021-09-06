d0d20d54b0cb061fe599dea4ac66f4dd780d1464
Dhruv Bhanushali <dhruv_b@live.com>
2021-09-06 23:15:13 +0530

    Configure ESLint for `script setup` quirks

--- src/components/.eslintrc.js
+++ src/components/.eslintrc.js
@@ -1,9 +1,21 @@
 module.exports = {
   extends: '../../.eslintrc.js',
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+  },
   rules: {
     'unicorn/filename-case': [
       'error',
       { case: 'pascalCase', ignore: ['.eslintrc.js'] },
     ],
   },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'no-unused-vars': 'off',
+      },
+    },
+  ],
 }
