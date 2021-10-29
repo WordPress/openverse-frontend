0b974240ede5763aa33c6f5de24931e619e090cf
Krystle Salazar <krystle.salazar@ciens.ucv.ve>
2021-10-29 17:48:09 -0400

    Add base square skeleton grid

---
+++ src/components/Skeleton/SkeletonGrid.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="p-4 grid tab:grid-cols-2 desk:grid-cols-5 gap-4">
+    <div v-for="idx in 20" :key="idx" class="square bg-dark-charcoal-10">
+      &nbsp;
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SkeletonGrid',
+}
+</script>
+
+<style lang="scss" scoped>
+.square {
+  aspect-ratio: 1 / 1;
+}
+</style>
