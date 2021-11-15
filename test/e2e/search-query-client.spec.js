f8b0b91276ae8dc264f32ce87ec3b4692c566553
Olga Bulat <obulat@gmail.com>
2021-11-15 20:11:20 +0300

    Add e2e test for search term change correctly

--- test/e2e/search-query-client.spec.js
+++ test/e2e/search-query-client.spec.js
@@ -103,7 +103,7 @@ test.skip('url filter types not used by current mediaType are discarded', async 
 
 test('can search for a different term', async ({ page }) => {
   await page.goto('/search/image?q=cat')
-  await page.type('input[type="search"]', 'dog')
+  await page.fill('input[type="search"]', 'dog')
   await page.keyboard.press('Enter')
   await expect(page).toHaveURL('/search/image?q=dog')
 })
