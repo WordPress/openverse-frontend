cde1c27864f784ae6802e327351e64eb568f157c
Krystle Salazar <krystle.salazar@ciens.ucv.ve>
2021-12-02 12:49:06 -0400

    Add e2e test for navigation to image detail page

--- test/e2e/search.spec.js
+++ test/e2e/search.spec.js
@@ -55,3 +55,11 @@ test('shows search result metadata', async ({ page }) => {
   // All search results have been shown, cannot load more
   await expect(loadMoreButton).toHaveCount(0)
 })
+
+test('navigates to the image details page correctly', async ({ page }) => {
+  await page.goto('/search/image?q=honey')
+  const figure = page.locator('figure').first()
+  const imgTitle = await figure.locator('img').getAttribute('alt')
+  await figure.click()
+  await expect(page.locator('h1')).toHaveText(imgTitle)
+})
