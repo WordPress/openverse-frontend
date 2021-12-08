94fa1721b4355b8d20240dedbc373e2e14095a43
Dhruv Bhanushali <dhruv_b@live.com>
2021-12-08 08:30:51 +0400

    Use `Array.reduce` instead of `Object.fromEntries`
    Co-authored-by: sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>

--- src/data/base-media-service.js
+++ src/data/base-media-service.js
@@ -2,12 +2,10 @@ import decodeMediaData from '~/utils/decode-media-data.js'
 
 const BaseMediaService = (mediaType) => ({
   transformResults(data) {
-    data.results = Object.fromEntries(
-      data.results.map((item) => {
-        item = decodeMediaData(item, mediaType)
-        return [item.id, item]
-      })
-    )
+    data.results = data.results.reduce((acc, item) => {
+      acc[item.id] = decodeMediaData(item, mediaType)
+      return acc
+    }, {})
     return data
   },
 })
