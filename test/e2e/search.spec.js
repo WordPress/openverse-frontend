c2951b83ffd3564f0d0e0638c7bab28cbaa5590d
Olga Bulat <obulat@gmail.com>
2021-12-14 18:44:10 +0300

    Fix exported name not changed, e2e tests

--- test/e2e/search.spec.js
+++ test/e2e/search.spec.js
@@ -51,6 +51,9 @@ test('shows search result metadata', async ({ page }) => {
   await expect(loadMoreButton).toHaveCount(1)
   await loadMoreButton.click()
 
+  // Click load more button
+  await expect(loadMoreButton).toHaveCount(1)
+  await loadMoreButton.click()
   // All search results have been shown, cannot load more
   await expect(loadMoreButton).toHaveCount(0)
 })
@@ -59,7 +62,8 @@ test('navigates to the image detail page correctly', async ({ page }) => {
   await page.goto('/search/image?q=honey')
   const figure = page.locator('figure').first()
   const imgTitle = await figure.locator('img').getAttribute('alt')
-  await figure.click()
+
+  await page.locator('figure a').first().click()
   await expect(page.locator('h1')).toHaveText(imgTitle)
   // Renders the breadcrumb link
   await expect(page.locator('text="Back to search results"')).toBeVisible()
