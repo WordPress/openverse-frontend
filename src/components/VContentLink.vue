7b19dfa4f4997ef3a1392e41f5495dd553f322f5
Krystle Salazar <krystle.salazar@ciens.ucv.ve>
2021-12-23 19:01:32 -0400

    Add `VContentLink` component

---
+++ src/components/VContentLink.vue
@@ -0,0 +1,58 @@
+<template>
+  <button
+    class="border border-dark-charcoal/25 rounded-sm bg-white flex flex-col text-left p-4"
+  >
+    <VIcon :icon-path="iconPath" />
+    <p class="capitalize font-semibold">{{ mediaType }}</p>
+    <span>{{ resultsCountLabel }}</span>
+  </button>
+</template>
+
+<script>
+import { computed, defineComponent, useContext } from '@nuxtjs/composition-api'
+import { AUDIO, IMAGE, mediaTypes } from '~/constants/media'
+import VIcon from '~/components/VIcon/VIcon.vue'
+
+import audioIcon from '~/assets/icons/audio-wave.svg'
+import imageIcon from '~/assets/icons/image.svg'
+
+const iconMapping = {
+  [AUDIO]: audioIcon,
+  [IMAGE]: imageIcon,
+}
+
+export default defineComponent({
+  name: 'VContentLink',
+  components: { VIcon },
+  props: {
+    mediaType: {
+      type: String,
+      required: true,
+      validator: (val) => mediaTypes.includes(val),
+    },
+    resultsCount: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const { i18n } = useContext()
+
+    /**
+     * The translated string showing how many results were found for this media type.
+     *
+     * @returns {string}
+     */
+    const resultsCountLabel = computed(() => {
+      const count = props.resultsCount
+      const i18nKey = 'browse-page.all-result-count'
+      const localeCount = count.toLocaleString(i18n.locale)
+      return i18n.tc(i18nKey, count, { localeCount })
+    })
+
+    const iconPath = computed(() => iconMapping[props.mediaType])
+
+    return { iconPath, imageIcon, resultsCountLabel }
+  },
+})
+</script>
