dc59f1ff0021cef53c97638bbc0a3647d1bcba95
Krystle Salazar <krystle.salazar@ciens.ucv.ve>
2022-01-04 17:49:52 -0400

    Add Storybook story for mobile view

--- src/components/VContentLink/meta/VContentLink.stories.js
+++ src/components/VContentLink/meta/VContentLink.stories.js
@@ -20,8 +20,34 @@ const VContentLinkStory = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
 })
 
+const TwoVContentLinkStory = () => ({
+  template: `
+  <div class="grid grid-cols-2 gap-4">
+    <VContentLink
+      v-for="(type, key) in types"
+      :media-type="type.mediaType"
+      :results-count="type.resultsCount"
+      :key="key"/>
+  </div>
+  `,
+  components: { VContentLink },
+  data() {
+    return {
+      types: [
+        { mediaType: 'image', resultsCount: 654321 },
+        { mediaType: 'audio', resultsCount: 123456 },
+      ],
+    }
+  },
+})
+
 export const Default = VContentLinkStory.bind({})
 Default.args = {
   mediaType: 'image',
   resultsCount: 123456,
+}
+
+export const Mobile = TwoVContentLinkStory.bind({})
+Mobile.parameters = {
+  viewport: { defaultViewport: 'mob' },
 }
