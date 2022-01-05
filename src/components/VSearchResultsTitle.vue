24c3c7568b5c197c55a780f6487a468634534f49
Zack Krida <zackkrida@pm.me>
2022-01-05 16:27:43 -0500

    init

---
+++ src/components/VSearchResultsTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1
+    class="capitalize leading-tight"
+    :class="{ 'text-[90px]': size === 'large' }"
+  >
+    <slot default />
+  </h1>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+const sizes = {
+  DEFAULT: 'default',
+  LARGE: 'large',
+}
+const VSearchResultsTitle = defineComponent({
+  name: 'VSearchResultsTitle',
+  props: {
+    size: {
+      required: false,
+      default: sizes.DEFAULT,
+      validator: (value) => Object.values(sizes).includes(value),
+    },
+  },
+})
+export default VSearchResultsTitle
+</script>
