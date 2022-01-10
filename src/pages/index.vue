016090d6c96f2205e0f45a95052ae585e656627c
Zack Krida <zackkrida@pm.me>
2022-01-10 17:34:01 -0500

    early init

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,23 +1,108 @@
 <template>
-  <VHeroSection />
+  <main
+    class="bg-yellow h-screen flex justify-center items-center overflow-hidden"
+  >
+    <header class="flex flex-col justify-center w-1/2 xl:w-2/3 pl-4 xl:pl-64">
+      <h1>
+        <span class="sr-only">Openverse</span>
+        <OpenverseLogoText class="w-80" />
+      </h1>
+      <h2>Browse through over 600 million items to reuse</h2>
+      <label for="">
+        <span class="sr-only">Cool</span>
+        <input type="text" />
+      </label>
+      <p>
+        All Openverse content is under a creative Comons license or is in the
+        public domain.
+      </p>
+    </header>
+    <div class="images w-1/2 xl:w-1/3 grid grid-cols-2 grid-rows-4">
+      <div
+        v-for="(image, index) in featuredSearch.images"
+        :key="index"
+        class="p-4 xl:p-8 block aspect-square"
+      >
+        <NuxtLink
+          :to="image.url"
+          class="aspect-square block rounded-full overflow-hidden h-full w-full"
+        >
+          <img
+            class="object-cover h-full w-full"
+            :src="image.src"
+            :alt="image.alt"
+          />
+        </NuxtLink>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import { SEARCH } from '~/constants/store-modules'
-import { CLEAR_FILTERS } from '~/constants/action-types'
-
-import VHeroSection from '~/components/VHeroSection.vue'
+import OpenverseLogoText from '~/assets/icons/openverse-logo-text.svg?inline'
 
 const HomePage = {
   name: 'home-page',
-  layout: 'default',
+  layout: 'blank',
   components: {
-    VHeroSection,
+    OpenverseLogoText,
   },
-  beforeMount() {
-    this.$store.dispatch(`${SEARCH}/${CLEAR_FILTERS}`)
+  setup() {
+    const featuredSearches = [
+      {
+        term: 'Indigenous pottery',
+        images: [
+          {
+            url: '/about',
+            src: 'https://source.unsplash.com/1600x900/?party',
+            alt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          },
+          {
+            url: '/about',
+            src: 'https://source.unsplash.com/1600x900/?party',
+            alt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          },
+          {
+            url: '/about',
+            src: 'https://source.unsplash.com/1600x900/?party',
+            alt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          },
+          {
+            url: '/about',
+            src: 'https://source.unsplash.com/1600x900/?party',
+            alt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          },
+          {
+            url: '/about',
+            src: 'https://source.unsplash.com/1600x900/?party',
+            alt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          },
+          {
+            url: '/about',
+            src: 'https://source.unsplash.com/1600x900/?party',
+            alt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          },
+          {
+            url: '/about',
+            src: 'https://source.unsplash.com/1600x900/?party',
+            alt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          },
+        ],
+      },
+    ]
+    const featuredSearch = featuredSearches[0]
+
+    return {
+      featuredSearch,
+    }
   },
 }
 
 export default HomePage
 </script>
+
+<style scoped>
+.images > *:nth-child(even) {
+  transform: translateY(50%);
+}
+</style>
