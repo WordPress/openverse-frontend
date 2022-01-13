602055072014fb6c3a39c691928779c3d4238b51
Olga Bulat <obulat@gmail.com>
2022-01-13 14:51:50 +0300

    Fix result count in VHeader

--- src/components/VHeader/VHeader.vue
+++ src/components/VHeader/VHeader.vue
@@ -176,9 +176,11 @@ const VHeader = defineComponent({
      * @returns {string}
      */
     const mediaCount = (count) => {
+      if (store.getters['media/unsupportedMediaType']) return ''
+
       const countKey =
         count === 0 ? 'noResult' : count >= 10000 ? 'more' : 'result'
-      const i18nKey = i18nKeys[store.state.search.query.mediaType][countKey]
+      const i18nKey = i18nKeys[store.getters['media/mediaType']][countKey]
       const localeCount = count.toLocaleString(i18n.locale)
       return i18n.tc(i18nKey, count, { localeCount })
     }
