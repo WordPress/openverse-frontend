4a8107e37a37b38bbf15201b48ba1b05fd96660a
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-01-20 12:50:01 -0500

    Delay loading audio until seeked or played and fix page transitions

--- src/components/VAudioTrack/VAudioTrack.vue
+++ src/components/VAudioTrack/VAudioTrack.vue
@@ -35,7 +35,7 @@ import {
   useStore,
   watch,
   onUnmounted,
-  onMounted,
+  useRoute,
 } from '@nuxtjs/composition-api'
 
 import { useActiveAudio } from '~/composables/use-active-audio'
@@ -48,7 +48,7 @@ import VRowLayout from '~/components/VAudioTrack/layouts/VRowLayout.vue'
 import VBoxLayout from '~/components/VAudioTrack/layouts/VBoxLayout.vue'
 import VGlobalLayout from '~/components/VAudioTrack/layouts/VGlobalLayout.vue'
 
-import { ACTIVE } from '~/constants/store-modules'
+import { ACTIVE, MEDIA } from '~/constants/store-modules'
 import {
   PAUSE_ACTIVE_MEDIA_ITEM,
   SET_ACTIVE_MEDIA_ITEM,
@@ -59,7 +59,7 @@ const propTypes = {
    * the information about the track, typically from a track's detail endpoint
    */
   audio: {
-    type: /** @type {import('@nuxtjs/composition-api').PropType<{ url: string, duration: number, id: string }>} */ (
+    type: /** @type {import('@nuxtjs/composition-api').PropType<import('~/store/types').AudioDetail>} */ (
       Object
     ),
     required: /** @type {true} */ (true),
@@ -116,67 +116,15 @@ export default defineComponent({
    */
   setup(props) {
     const store = useStore()
+    const route = useRoute()
 
     const activeAudio = useActiveAudio()
-    /**
-     * We can only create the local audio object on the client,
-     * so the initialization of this variable is hidden inside
-     * the `onMounted` hook.
-     *
-     * Rather than initialize everything for this component
-     * inside the onMounted hook, we can just use a closure
-     * around this variable to make everything else present
-     * on the top level of the setup function's scope and limit
-     * the behavior of the `onMounted` hook to just initializing
-     * the audio object for this instance.
-     *
-     * However, when navigating to an audio result page, if
-     * the globally active audio already matches the result
-     * that was clicked on, hijack that object instead and
-     * treat it as the local audio for this instance.
-     *
-     * @type {HTMLAudioElement | undefined}
-     * */
-    let localAudio =
-      activeAudio.obj.value?.src === props.audio.url
-        ? activeAudio.obj.value
-        : undefined
 
     const status = ref('paused')
     const currentTime = ref(0)
 
-    const updateTimeLoop = () => {
-      if (localAudio && status.value === 'playing') {
-        currentTime.value = localAudio.currentTime
-        window.requestAnimationFrame(updateTimeLoop)
-      }
-    }
-
-    const setPlaying = () => {
-      status.value = 'playing'
-      activeAudio.obj.value = localAudio
-      store.commit(`${ACTIVE}/${SET_ACTIVE_MEDIA_ITEM}`, {
-        type: 'audio',
-        id: props.audio.id,
-      })
-      updateTimeLoop()
-    }
-    const setPaused = () => {
-      status.value = 'paused'
-      store.commit(`${ACTIVE}/${PAUSE_ACTIVE_MEDIA_ITEM}`)
-    }
-    const setPlayed = () => (status.value = 'played')
-    const setTimeWhenPaused = () => {
-      if (status.value !== 'playing' && localAudio) {
-        currentTime.value = localAudio.currentTime
-        if (status.value === 'played') {
-          // Set to pause to remove replay icon
-          status.value = 'paused'
-        }
-      }
-    }
-
-    onMounted(() => {
+    const initLocalAudio = () => {
+      // Preserve existing local audio if we plucked it from the global active audio
       if (!localAudio) localAudio = new Audio(props.audio.url)
 
       localAudio.addEventListener('play', setPlaying)
@@ -216,16 +164,103 @@ export default defineComponent({
       }
 
       currentTime.value = localAudio.currentTime
-    })
+    }
+
+    /**
+     * We can only create the local audio object on the client,
+     * so the initialization of this variable is hidden inside
+     * the `initLocalAudio` function which is only called when
+     * playback is first requested or when the track if first seeked.
+     *
+     * However, when navigating to an audio result page, if
+     * the globally active audio already matches the result
+     * that was clicked on, hijack that object instead and
+     * treat it as the local audio for this instance.
+     *
+     * @type {HTMLAudioElement | undefined}
+     * */
+    let localAudio =
+      activeAudio.obj.value?.src === props.audio.url
+        ? activeAudio.obj.value
+        : undefined
+
+    const updateTimeLoop = () => {
+      if (localAudio && status.value === 'playing') {
+        currentTime.value = localAudio.currentTime
+        window.requestAnimationFrame(updateTimeLoop)
+      }
+    }
+
+    const setPlaying = () => {
+      status.value = 'playing'
+      activeAudio.obj.value = localAudio
+      store.commit(`${ACTIVE}/${SET_ACTIVE_MEDIA_ITEM}`, {
+        type: 'audio',
+        id: props.audio.id,
+      })
+      updateTimeLoop()
+    }
+    const setPaused = () => {
+      status.value = 'paused'
+      store.commit(`${ACTIVE}/${PAUSE_ACTIVE_MEDIA_ITEM}`)
+    }
+    const setPlayed = () => (status.value = 'played')
+    const setTimeWhenPaused = () => {
+      if (status.value !== 'playing' && localAudio) {
+        currentTime.value = localAudio.currentTime
+        if (status.value === 'played') {
+          // Set to pause to remove replay icon
+          status.value = 'paused'
+        }
+      }
+    }
+
+    /**
+     * If we're transforming the globally active audio
+     * into our local audio, then we need to initialize
+     * the local state syncing from the audio object
+     * to our local refs.
+     */
+    if (localAudio) initLocalAudio()
 
     onUnmounted(() => {
-      localAudio?.removeEventListener('play', setPlaying)
-      localAudio?.removeEventListener('pause', setPaused)
-      localAudio?.removeEventListener('ended', setPlayed)
-      localAudio?.removeEventListener('timeupdate', setTimeWhenPaused)
+      if (!localAudio) return
+
+      localAudio.removeEventListener('play', setPlaying)
+      localAudio.removeEventListener('pause', setPaused)
+      localAudio.removeEventListener('ended', setPlayed)
+      localAudio.removeEventListener('timeupdate', setTimeWhenPaused)
+
+      if (
+        route.value.params.id == props.audio.id ||
+        store.getters[`${MEDIA}/results`]?.items?.[props.audio.id]
+      ) {
+        /**
+         * If switching to any route other than the single result
+         * route for this track, pause it. Otherwise, let it keep
+         * playing to introduce a "seamless" feeling beween the
+         * search results page and the single result page.
+         *
+         * This handles going from the search page to the single
+         * result page for a different track than is currently playing.
+         * It also handles the same interaction for related audio.
+         * Also for related audio, it will handle pausing any related
+         * audio when navigating back from the single result page
+         * to the search results page.
+         *
+         * Also, if the currently playing audio is present in the
+         * existing list of search results, then also let it keep
+         * playing.
+         */
+        return
+      }
+
+      localAudio.pause()
     })
 
     const play = () => {
+      // delay initializing the local audio element until playback is requested
+      if (!localAudio) initLocalAudio()
       localAudio?.play()
     }
     const pause = () => localAudio?.pause()
@@ -268,7 +303,15 @@ export default defineComponent({
      * @param {number} frac
      */
     const handleSeeked = (frac) => {
-      if (!localAudio) return
+      if (!localAudio) initLocalAudio()
+      /**
+       * Calling initLocalAudio will guarantee localAudio
+       * to be an HTMLAudioElement, but we can't prove that
+       * to TypeScript without jumping through some tricky
+       * hoops (using `assert`) or adding unnecessary
+       * runtime checks.
+       */
+      // @ts-ignore
       localAudio.currentTime = frac * duration.value
     }
 
