7df239c0c057e9d78771136dbb8f55bf98a203c6
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-02-01 10:31:05 -0500

    Add VueI18n to Nuxt context and type check new composable

--- src/composables/use-get-locale-formatted-number.js
+++ src/composables/use-get-locale-formatted-number.js
@@ -6,8 +6,6 @@ export const EASTERN_ARABIC_NUMERAL_LOCALES = ['ar', 'fa', 'ckb', 'ps']
  * Guards number formatting to prevent using Eastern Arabic Numerals,
  * instead always using Western Arabic Numerals but still respecting
  * the locale preferences for delimiters.
- *
- * @returns {(n: number) => string}
  */
 export const useGetLocaleFormattedNumber = () => {
   const { i18n } = useContext()
