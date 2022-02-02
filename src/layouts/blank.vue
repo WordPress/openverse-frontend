c0138e1a8421b761629c7778004ff308a4026f24
Zack Krida <zackkrida@pm.me>
2022-02-02 08:46:08 -0500

    Add RTL and CC banner support to blank layout

--- src/layouts/blank.vue
+++ src/layouts/blank.vue
@@ -1,3 +1,36 @@
 <template>
-  <Nuxt />
+  <div>
+    <MigrationNotice v-show="isReferredFromCc" />
+    <TranslationStatusBanner />
+    <Nuxt ref="mainContentRef" />
+  </div>
 </template>
+<script>
+import iframeHeight from '~/mixins/iframe-height'
+
+import { NAV } from '~/constants/store-modules'
+import { useContext } from '@nuxtjs/composition-api'
+
+import MigrationNotice from '~/components/MigrationNotice.vue'
+import TranslationStatusBanner from '~/components/TranslationStatusBanner.vue'
+
+const embeddedPage = {
+  name: 'embedded',
+  components: {
+    MigrationNotice,
+    TranslationStatusBanner,
+  },
+  layout: 'embedded',
+  mixins: [iframeHeight],
+  head() {
+    return this.$nuxtI18nHead({ addSeoAttributes: true, addDirAttribute: true })
+  },
+  setup() {
+    const { store } = useContext()
+    const isReferredFromCc = store.state[NAV].isReferredFromCc
+
+    return { isReferredFromCc }
+  },
+}
+export default embeddedPage
+</script>
