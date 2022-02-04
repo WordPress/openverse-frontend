6542037bb77ad8156ff7e33d6c33bf33d4dd03d5
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-04 08:18:28 +0400

    Update color 'primary' to 'pink' after redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
       // Brand
       yellow: '#ffe033',
       pink: '#c52b9b',
-      primary: '#3e58e1', // will change to pink after redesign launch
+      primary: '#c52b9b', // TODO: Change usages to 'pink'
       // Active
       'dark-pink': '#7c2264',
 
