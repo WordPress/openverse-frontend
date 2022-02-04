3300e762523afb198a646e3798cdfa912f333430
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-04 08:54:26 +0400

    Create the content report flow components

---
+++ src/components/VContentReport/VContentReportPopover.vue
@@ -0,0 +1,71 @@
+<template>
+  <VPopover
+    ref="popoverEl"
+    :hide-on-click-outside="false"
+    placement="bottom-end"
+    :z-index="20"
+  >
+    <template #trigger="{ a11yProps }">
+      <VContentReportButton v-bind="a11yProps" />
+    </template>
+    <div class="relative">
+      <VContentReportForm :media="media" :provider-name="providerName" />
+      <VIconButton
+        class="absolute top-0 end-0 border-none"
+        size="search-medium"
+        :icon-props="{ iconPath: icons.closeSmall }"
+        @click="handleClose"
+      />
+    </div>
+  </VPopover>
+</template>
+
+<script>
+import {
+  ref,
+  computed,
+  useStore,
+  defineComponent,
+} from '@nuxtjs/composition-api'
+
+import VPopover from '~/components/VPopover/VPopover.vue'
+import VContentReportButton from '~/components/VContentReport/VContentReportButton.vue'
+
+import flagIcon from '~/assets/icons/flag.svg'
+import closeSmallIcon from '~/assets/icons/close-small.svg'
+
+export default defineComponent({
+  name: 'VContentReportPopover',
+  components: { VPopover, VContentReportButton },
+  props: {
+    /**
+     * the media item to report; This can either be an audio track or an image.
+     */
+    media: {
+      type: Object,
+    },
+  },
+  setup(props) {
+    const store = useStore()
+
+    const popoverEl = ref(null)
+    const handleClose = () => {
+      // TODO: Hack till https://github.com/WordPress/openverse-frontend/issues/725
+      popoverEl.value.close()
+    }
+
+    const getProviderName = (nameCode) =>
+      store.getters['provider/getProviderName'](nameCode)
+    const providerName = computed(() => getProviderName(props.media.provider))
+
+    return {
+      icons: { flag: flagIcon, closeSmall: closeSmallIcon },
+
+      popoverEl,
+      handleClose,
+
+      providerName,
+    }
+  },
+})
+</script>
--- src/components/VContentReport/VOtherIssueForm.vue
+++ src/components/VContentReport/VOtherIssueForm.vue
@@ -1,71 +1,49 @@
 <template>
-  <form class="other-form">
-    <h4 class="b-header">
-      {{ $t('photo-details.content-report.title') }}
-    </h4>
-    <label for="description" class="mb-2">
-      {{ $t('photo-details.content-report.issue-description') }}
-      <textarea
-        id="description"
-        v-model="description"
-        class="reason p-2 font-semibold border border-dark-charcoal w-full h-24 text-sm"
-        :placeholder="$t('photo-details.content-report.placeholder')"
-      />
-    </label>
-
-    <div class="flex flex-row justify-between align-end mt-4">
-      <VButton
-        type="button"
-        variant="action-button"
-        size="small"
-        @click="onBackClick"
-      >
-        <VIcon :icon-path="chevronLeftIcon" class="me-2" />
-        {{ $t('photo-details.content-report.back') }}
-      </VButton>
-
-      <VButton
-        :disabled="!isValid"
-        variant="secondary"
-        class="float-right bg-trans-blue"
-        @click="sendContentReport"
-      >
-        {{ $t('photo-details.content-report.submit') }}
-      </VButton>
+  <div class="other-form">
+    <div class="flex flex-row items-center justify-between">
+      <label for="description">{{
+        $t('media-details.content-report.form.other.note')
+      }}</label>
+      <span class="text-dark-charcoal-70">{{ text.length }}</span>
     </div>
-  </form>
+
+    <!-- eslint-disable vuejs-accessibility/form-control-has-label -->
+    <textarea
+      id="description"
+      v-model="text"
+      class="h-20 w-full border border-dark-charcoal-20 placeholder-dark-charcoal-70 mt-2 p-2"
+      :placeholder="$t('media-details.content-report.form.other.minimum')"
+    />
+    <!-- eslint-enable -->
+  </div>
 </template>
 
 <script>
-import { computed, defineComponent, ref } from '@nuxtjs/composition-api'
-import chevronLeftIcon from '~/assets/icons/chevron-left.svg'
-import VButton from '~/components/VButton.vue'
-import VIcon from '~/components/VIcon/VIcon.vue'
+import { computed, defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
-  name: 'OtherIssueForm',
-  components: {
-    VButton,
-    VIcon,
+  name: 'VOtherIssueForm',
+  model: {
+    prop: 'content',
+    event: 'input',
   },
-  setup(_, { emit }) {
-    const onBackClick = () => {
-      emit('back-click')
-    }
-    const description = ref('')
-    const descriptionHasMoreThan20Chars = computed(
-      () => description.value.length >= 20
-    )
+  props: {
+    /**
+     * the contents of the textarea
+     */
+    content: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props, { emit }) {
+    const text = computed({
+      get: () => props.content,
+      set: (val) => emit('input', val),
+    })
 
-    const sendContentReport = () =>
-      emit('send-report', { description: description.value })
     return {
-      chevronLeftIcon,
-      description,
-      isValid: descriptionHasMoreThan20Chars,
-
-      onBackClick,
-      sendContentReport,
+      text,
     }
   },
 })
