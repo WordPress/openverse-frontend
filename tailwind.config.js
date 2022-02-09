b00007a94328563441dea37b39d86041aad4cb23
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-09 17:06:50 +0400

    Change uses of primary to pink

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,6 @@ module.exports = {
       // Brand
       yellow: '#ffe033',
       pink: '#c52b9b',
-      primary: '#c52b9b', // TODO: Change usages to 'pink'
       // Active
       'dark-pink': '#7c2264',
 
