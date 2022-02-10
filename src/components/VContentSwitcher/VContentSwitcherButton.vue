34f8805bebe83b27bbc9b882c919d0394b2f7548
Olga Bulat <obulat@gmail.com>
2022-02-10 13:23:02 +0300

    Set fixed width for the content switcher based on the longest i18n string

--- src/components/VContentSwitcher/VContentSwitcherButton.vue
+++ src/components/VContentSwitcher/VContentSwitcherButton.vue
@@ -9,9 +9,19 @@
     @click="$emit('click')"
   >
     <VIcon :icon-path="icon" />
-    <span v-show="showLabel" :class="{ 'ms-2 md:w-20 text-left': showLabel }">{{
-      buttonLabel
-    }}</span>
+    <p
+      v-show="showLabel"
+      :class="{ 'ms-2 text-left': showLabel }"
+      :style="labelFixedWidthStyle"
+    >
+      {{ buttonLabel }}
+    </p>
+    <span
+      v-if="showLabel"
+      ref="testLabel"
+      class="absolute invisible h-auto w-auto whitespace-nowrap"
+      >{{ longestLabel }}</span
+    >
     <VIcon
       class="hidden md:block text-dark-charcoal-40 md:ms-2"
       :icon-path="caretDownIcon"
@@ -19,8 +29,14 @@
   </VButton>
 </template>
 <script>
-import { ALL_MEDIA, AUDIO, IMAGE } from '~/constants/media'
-import { computed, inject, useContext } from '@nuxtjs/composition-api'
+import { ALL_MEDIA, supportedContentTypes } from '~/constants/media'
+import {
+  computed,
+  inject,
+  onMounted,
+  ref,
+  useContext,
+} from '@nuxtjs/composition-api'
 import useContentType from '~/composables/use-content-type'
 import { isMinScreen } from '~/composables/use-media-query'
 
@@ -40,10 +56,11 @@ export default {
     activeItem: {
       type: String,
       default: ALL_MEDIA,
-      validator: (val) => [ALL_MEDIA, IMAGE, AUDIO].includes(val),
+      validator: (val) => supportedContentTypes.includes(val),
     },
   },
   setup(props) {
+    const testLabel = ref(null)
     const { i18n } = useContext()
     const isHeaderScrolled = inject('isHeaderScrolled', null)
     const isMinScreenMd = isMinScreen('md', { shouldPassInSSR: true })
@@ -59,6 +76,24 @@ export default {
       return isMinScreenMd.value && !isHeaderScrolled?.value
         ? 'tertiary'
         : 'action-menu'
+    })
+    /**
+     * Get the longest translated label, assuming that the word that has the most characters is the longest.
+     */
+    const longestLabel = computed(() => {
+      const translatedLabels = supportedContentTypes
+        .map((type) => `search-type.${type}`)
+        .map((key) => i18n.t(key))
+      const maxLength = Math.max(
+        ...translatedLabels.map((label) => label.length || 0)
+      )
+      return translatedLabels.find((label) => label.length === maxLength)
+    })
+
+    const labelFixedWidthStyle = ref(' width:80px;')
+    onMounted(() => {
+      const widthInPx = testLabel.value.clientWidth
+      labelFixedWidthStyle.value = `width:${widthInPx}px;`
     })
     const buttonLabel = computed(() => {
       const labelKey = {
@@ -77,6 +112,9 @@ export default {
       buttonVariant,
       sizeClasses,
       buttonLabel,
+      longestLabel,
+      labelFixedWidthStyle,
+      testLabel,
       caretDownIcon,
       showLabel,
       isHeaderScrolled,
