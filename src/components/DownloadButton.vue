e8d70c66bdd00cfcf876f25767886a50277bcb2f
Olga Bulat <obulat@gmail.com>
2022-02-15 18:03:19 +0300

    Revert DownloadButton change

--- src/components/DownloadButton.vue
+++ src/components/DownloadButton.vue
@@ -6,7 +6,7 @@
     :size="size"
   >
     <template #default="{ buttonProps }">
-      <VLink
+      <a
         v-bind="buttonProps"
         class="whitespace-nowrap"
         :href="selectedFormat.download_url"
@@ -19,7 +19,7 @@
         <span class="ms-1 font-normal">{{
           getFormatSize(selectedFormat.extension_name)
         }}</span>
-      </VLink>
+      </a>
     </template>
 
     <template
