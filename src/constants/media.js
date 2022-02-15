61ff5955328ee9dc6892232e1997ede7c8863582
Olga Bulat <obulat@gmail.com>
2022-02-15 20:34:18 +0300

    Update src/constants/media.js
    Co-authored-by: sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>

--- src/constants/media.js
+++ src/constants/media.js
@@ -26,7 +26,7 @@ export const statuses = {
   ADDITIONAL: 'additional',
 }
 
-/** @type {Record.<MediaType, SupportStatus>} */
+/** @type {Record<MediaType, SupportStatus>} */
 export const contentStatus = {
   [ALL_MEDIA]: statuses.SUPPORTED,
   [IMAGE]: statuses.SUPPORTED,
