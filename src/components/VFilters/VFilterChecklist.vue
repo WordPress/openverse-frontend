0af7e0dd90985121579cce26d5c4f70985e524a2
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-15 15:04:25 +0400

    Add a close button to the license popover

--- src/components/VFilters/VFilterChecklist.vue
+++ src/components/VFilters/VFilterChecklist.vue
@@ -31,11 +31,19 @@
             class="text-dark-charcoal-70"
             type="button"
           >
-            <VIcon :icon-path="helpIcon" />
+            <VIcon :icon-path="icons.help" />
           </button>
         </template>
-        <template #default>
-          <VLicenseExplanation :license="item.code" />
+        <template #default="{ close }">
+          <div class="relative">
+            <VIconButton
+              class="absolute top-0 end-0 border-none"
+              size="search-medium"
+              :icon-props="{ iconPath: icons.closeSmall }"
+              @click="close"
+            />
+            <VLicenseExplanation :license="item.code" />
+          </div>
         </template>
       </VPopover>
     </div>
@@ -43,13 +51,14 @@
 </template>
 
 <script>
-import helpIcon from '~/assets/icons/help.svg'
-
 import VLicenseExplanation from '~/components/VFilters/VLicenseExplanation.vue'
 import VCheckbox from '~/components/VCheckbox/VCheckbox.vue'
 import VLicense from '~/components/License/VLicense.vue'
 import VIcon from '~/components/VIcon/VIcon.vue'
 import VPopover from '~/components/VPopover/VPopover.vue'
+
+import helpIcon from '~/assets/icons/help.svg'
+import closeSmallIcon from '~/assets/icons/close-small.svg'
 
 export default {
   name: 'FilterCheckList',
@@ -70,7 +79,8 @@ export default {
     return {
       licenseExplanationVisible: false,
       licenseExplanationCode: null,
-      helpIcon,
+
+      icons: { help: helpIcon, closeSmall: closeSmallIcon },
     }
   },
   computed: {
