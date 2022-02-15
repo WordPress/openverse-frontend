ebf52ff860aaccccf1325e4ea4695d29ba7680ef
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-15 15:02:58 +0400

    Fix non-existent max width

--- src/components/VFilters/VLicenseExplanation.vue
+++ src/components/VFilters/VLicenseExplanation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="license-explanation w-full max-w-80 p-6">
+  <div class="license-explanation w-full max-w-xs p-6">
     <h5 class="text-base font-semibold">
       <template v-if="isTechnicallyLicense">{{
         $t('filters.license-explanation.license-definition')
