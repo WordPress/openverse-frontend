4d7dfa06ef7f5a2cbec828e96fbc501933958c50
Olga Bulat <obulat@gmail.com>
2022-02-17 06:11:26 +0300

    Use href prop instead of href as an attr

--- src/components/VLink.vue
+++ src/components/VLink.vue
@@ -6,50 +6,34 @@
 
 <script>
 /**
- * This is a wrapper component for all links. All VLinks have to have a valid `href` attr.
+ * This is a wrapper component for all links. All VLinks have to have a valid `href` prop.
  * Links with `href` starting with `/` are treated as internal links.
  *
  * Internal links use `NuxtLink` component with `to` attribute set to `localePath(href)`
  * External links use `a` element set to open in a new tab and not raise an error with the current iframe setup.
  */
-import {
-  computed,
-  defineComponent,
-  useContext,
-  watch,
-} from '@nuxtjs/composition-api'
-import { warn } from '@/utils/warn'
+import { computed, defineComponent, useContext } from '@nuxtjs/composition-api'
 
 const defaultProps = { target: '_blank', rel: 'noopener noreferrer' }
 
 export default defineComponent({
   name: 'VLink',
-  setup(_, { attrs }) {
+  props: {
+    href: {
+      type: String,
+      required: true,
+      validator: (v) => !['', '#'].includes(v),
+    },
+  },
+  setup(props) {
     const { app } = useContext()
-    const isInternal = computed(() => attrs.href?.startsWith('/'))
+    const isInternal = computed(() => props.href?.startsWith('/'))
     const linkComponent = computed(() => (isInternal.value ? 'NuxtLink' : 'a'))
 
-    let linkProperties = {}
-    watch(
-      [attrs],
-      ([attrs]) => {
-        // No need to declare `href` as an explicit prop as Vue preserves
-        // the `attrs` object reference between renders and updates the properties
-        // meaning we'll always have the latest values for the properties on the
-        // attrs object
-        if (!attrs.href || attrs.href === '#') {
-          warn('VLink is missing a valid `href` attribute.')
-        }
-        const { href, ...otherProperties } = attrs
-        linkProperties = isInternal.value
-          ? { ...otherProperties, to: app.localePath(href) }
-          : {
-              ...otherProperties,
-              ...defaultProps,
-              href,
-            }
-      },
-      { immediate: true }
+    let linkProperties = computed(() =>
+      isInternal.value
+        ? { to: app?.localePath(props.href) ?? props.href }
+        : { ...defaultProps, href: props.href }
     )
 
     return { linkProperties, linkComponent }
