61945b9e87f929823cb72b5e39eeb1ef51f86fd3
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-21 12:50:35 +0400

    Fix a lint error that Prettier did not catch locally

--- src/store/types.d.ts
+++ src/store/types.d.ts
@@ -127,7 +127,7 @@ export interface ActiveMediaState {
   type: 'image' | 'audio' | null
   id: string | null
   status: 'ejected' | 'playing' | 'paused' // 'ejected' means player is closed
-  message: string | null,
+  message: string | null
 }
 
 export interface MediaStoreResult {
