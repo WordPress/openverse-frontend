5acea54a99b8a6a98957664a13f134b1a5fdfa65
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-28 23:02:56 +0400

    Use `as const` to use the narrowest type for `env`

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,9 +2,9 @@
  * Default environment variables are set on this key. Defaults are fallbacks to existing env vars.
  * All boolean values should be designed to be false by default.
  */
-export const env: Record<string, string> = {
+export const env = {
   apiUrl: process.env.API_URL ?? 'https://api.openverse.engineering/v1/',
   filterStorageKey: 'openverse-filter-visibility',
   notificationStorageKey: 'openverse-show-notification',
   enableInternalAnalytics: process.env.ENABLE_INTERNAL_ANALYTICS ?? 'false',
-}
+} as const
