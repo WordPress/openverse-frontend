f93c2db531593d33c9def1facfbb3c9684b27d77
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-03-04 13:19:58 -0500

    Fix bad interpreter

--- bin/playwright.sh
+++ bin/playwright.sh
@@ -1,8 +1,8 @@
-#! /bin/bash'
+#! /bin/bash
 
 # todo: find a way to make this portable for Windows users so it can be used for the package.json script
 
-export PLAYWRIGHT_VERSION=$(pnpm ls --dev --depth=0 | grep playwright | awk '{print $2}')
+PLAYWRIGHT_VERSION=$(pnpm ls --dev --depth=0 | grep playwright | awk '{print $2}')
 TEST_TYPE=$1 docker-compose -f docker-compose.playwright.yml up \
     --abort-on-container-exit \
     --exit-code-from playwright
