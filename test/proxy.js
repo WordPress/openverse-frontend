cdb935491af602616f21ddb8a023b49e667eb9ac
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-03-04 22:41:45 -0500

    Handle the occasional brotli compressed response

--- test/proxy.js
+++ test/proxy.js
@@ -9,6 +9,7 @@
  * tests less flaky due to changes in the API or API data.
  */
 const process = require('process')
+const zlib = require('zlib')
 
 const talkback = require('talkback')
 
@@ -24,34 +25,60 @@ const recordMode = updatingTapes
   : talkback.Options.RecordMode.DISABLED
 console.log('Record mode: ', recordMode)
 
-const subUpstreamForProxy = (obj) => {
-  return Object.entries(obj).reduce(
-    (acc, [key, val]) => {
-      if (typeof val === 'string') {
-        acc[key] = val.replace(
-          /https?:\/\/api.openverse.engineering\/v1/,
-          'http://api:3000'
-        )
-      }
-
-      return acc
-    },
-    { ...obj }
-  )
-}
-
 /**
  * Transform any response values to use the talkback
- * proxy instead of pointing directly upstream
+ * proxy instead of pointing directly upstream for
+ * RESTful references.
+ *
+ * Ignore thumbnail and non-successful requests, those
+ * don't return JSON bodies so we can save them as-is.
+ *
+ * Sometimes we get a raw brotli buffer back from upstream
+ * and we need to decompress it. Sometimes we get the actual
+ * JSON. I'm fairly confident this has something to do with Cloudflare
+ * cached responses being compressed and others not? In any case,
+ * if the response comes back compressed then we need to
+ * decompress it, fix the upstream API references, and then
+ * compress it back before saving. We could mess with the
+ * `content-encoding` header instead of re-compressing it but
+ * I think that would sort of violate the contract talkback is meant
+ * to have and furthermore would eliminate a complexity in
+ * our stack that apparently exists in production. Given e2e
+ * tests should aim to test as close to production conditions as
+ * possible I think it makes sense to retain both the compressed
+ * and uncompressed responses.
+ *
+ * A note to future contributors: If you find that JSON.parse is
+ * complaining about unknown characters then you're either
+ * dealing with an error response that isn't being caught by
+ * the status check on the first line OR you've discovered
+ * another compression algorithm being used (or, of course, it
+ * could be something else entirely). Don't discount that it could
+ * be something else, but I'd check those two things first
+ * before digging elsewhere.
  *
  * @param tape
  */
 const tapeDecorator = (tape) => {
-  const responseBody = JSON.parse(tape.res.body.toString())
-  if (Array.isArray(responseBody.results)) {
-    responseBody.results = responseBody.results.map(subUpstreamForProxy)
-    tape.res.body = Buffer.from(JSON.stringify(responseBody))
+  if (tape.req.url.endsWith('/thumb/') || tape.res.status >= 399) return tape
+
+  let responseBody,
+    isBrotli = tape.res.headers['content-encoding']?.includes('br')
+
+  if (isBrotli) {
+    responseBody = zlib.brotliDecompressSync(tape.res.body).toString()
+  } else {
+    responseBody = tape.res.body.toString()
   }
+
+  const fixedResponseBody = responseBody.replace(
+    /https?:\/\/api.openverse.engineering\/v1/g,
+    'http://localhost:3000'
+  )
+
+  tape.res.body = isBrotli
+    ? zlib.brotliCompressSync(fixedResponseBody)
+    : Buffer.from(fixedResponseBody)
   return tape
 }
 
