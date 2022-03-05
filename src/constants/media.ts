fa5a823d3596aa6b65c47778f1e329167362c6c6
Dhruv Bhanushali <dhruv_b@live.com>
2022-03-05 12:27:47 +0400

    Convert `media` constants to TypeScript

---
+++ src/constants/media.ts
@@ -0,0 +1,33 @@
+/* Media types */
+
+export const AUDIO = 'audio'
+export const IMAGE = 'image'
+export const VIDEO = 'video'
+export const ALL_MEDIA = 'all'
+
+/**
+ * real media types that the API supports;
+ * - `ALL_MEDIA` is not an option here.
+ * - These types also support custom filters.
+ * - `IMAGE` should always be first here.
+ */
+export const supportedMediaTypes = [IMAGE, AUDIO] as const
+
+/**
+ * the types of content that users can search; `ALL_MEDIA` is also a valid
+ * option here.
+ */
+export const supportedSearchTypes = [ALL_MEDIA, IMAGE, AUDIO] as const
+
+/* Media support */
+
+const SUPPORTED = 'supported' // Native search
+const BETA = 'beta' // Native but incomplete search
+const ADDITIONAL = 'additional' // Meta search
+
+export const contentStatus = {
+  [ALL_MEDIA]: SUPPORTED,
+  [IMAGE]: SUPPORTED,
+  [AUDIO]: BETA,
+  [VIDEO]: ADDITIONAL,
+} as const
