ac8b2173c653514fbd761b9f2e9095b0b2cecc60
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-03-04 22:42:20 -0500

    Run app and proxy inside the playwright container

--- test/e2e/playwright.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
-  testDir: '.',
-  use: {
-    baseURL: 'http://web:8443',
-  },
-  timeout: 60 * 1000,
-}
-module.exports = config
