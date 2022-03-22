7c7893a7fcf72950b49dbef229cce0a4e5f61225
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-03-22 09:35:34 -0400

    Use utility for mocking provider APIs

--- test/playwright/utils/route.ts
+++ test/playwright/utils/route.ts
@@ -2,4 +2,5 @@ import type { BrowserContext } from '@playwright/test'
 
 export const mockProviderApis = async (context: BrowserContext) => {
   await context.route('**.jamendo.com**', (route) => route.abort())
+  await context.route('**.freesound.**', (r) => r.abort())
 }
