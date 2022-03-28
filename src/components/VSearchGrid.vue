97fb3d93caa46ccd8a35c8100d832fca7e3715a2
Olga Bulat <obulat@gmail.com>
2022-03-28 08:44:35 +0300

    Make sure a component is displayed in search.vue

--- src/components/VSearchGrid.vue
+++ src/components/VSearchGrid.vue
@@ -29,7 +29,7 @@
       :is-supported="supported"
     />
   </section>
-  <VErrorSection v-else-if="!fetchState.isFetching" class="w-full py-10">
+  <VErrorSection v-else class="w-full py-10">
     <template #image>
       <VErrorImage :error-code="NO_RESULT" />
     </template>
