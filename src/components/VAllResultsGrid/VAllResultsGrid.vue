99a13eaeaddfab8ec61875391f84b171c490c1f2
kb-0311 <kanishka.bansode@gmail.com>
2022-03-29 14:10:48 +0530

    removed template+renaming comp

--- src/components/VAllResultsGrid/VAllResultsGrid.vue
+++ src/components/VAllResultsGrid/VAllResultsGrid.vue
@@ -35,10 +35,9 @@
       </div>
     </div>
 
-    <template v-if="isError" class="m-auto w-1/2 text-center pt-6">
       <h5>{{ errorHeader }}</h5>
       <p>{{ fetchState.fetchingError }}</p>
-    </template>
+    
 
     <VLoadMore
       v-if="canLoadMore && !fetchState.isFinished"
