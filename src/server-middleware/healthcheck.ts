86a200ac0c5cc196e2fea8a3ada69725fe6d6bb0
Olga Bulat <obulat@gmail.com>
2022-04-04 09:43:07 +0300

    Revert changes to server-middleware

--- src/server-middleware/healthcheck.ts
+++
@@ -1,8 +0,0 @@
-import type { ServerMiddleware } from '@nuxt/types'
-
-const healthcheck: ServerMiddleware = (_req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('OK')
-}
-
-export default healthcheck
