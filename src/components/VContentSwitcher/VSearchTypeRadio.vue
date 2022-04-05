a63d6eba2d8cc7c3aa3d01fdabd7bbb3019ee902
Krystle Salazar <krystle.salazar@ciens.ucv.ve>
2022-04-04 22:59:20 -0400

    Apply `focus-visible` styles inline

--- src/components/VContentSwitcher/VSearchTypeRadio.vue
+++ src/components/VContentSwitcher/VSearchTypeRadio.vue
@@ -1,11 +1,11 @@
 <template>
   <button
     type="button"
-    class="flex flex-row items-center p-2 pe-3 rounded-sm text-sr font-semibold border border-dark-charcoal focus-visible:outline-none focus-visible:ring focus-visible:ring-pink hover:text-white hover:bg-dark-charcoal focus-visible:hover:border-white focus-visible:border-tx"
+    class="flex flex-row items-center p-2 pe-3 rounded-sm text-sr font-semibold border border-dark-charcoal hover:text-white hover:bg-dark-charcoal focus:outline-none focus-visible:ring focus-visible:ring-pink focus-visible:hover:border-white"
     :class="[
       selected
         ? 'text-white bg-dark-charcoal focus-visible:border focus-visible:border-white'
-        : 'bg-tx',
+        : 'bg-tx focus-visible:border-tx',
     ]"
     :aria-pressed="selected"
     @click="handleClick"
