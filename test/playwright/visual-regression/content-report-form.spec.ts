dadebee75e7cbbbbf764d6902682480424e1a703
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-04-15 10:13:05 +0200

    Increase timeout to prevent flakiness

--- test/playwright/visual-regression/content-report-form.spec.ts
+++ test/playwright/visual-regression/content-report-form.spec.ts
@@ -6,19 +6,19 @@ test.describe('content report form', () => {
   })
 
   test('unfocused close button', async ({ page }) => {
-    await page.click('button:has-text("report")', { timeout: 100 })
+    await page.click('button:has-text("report")', { timeout: 500 })
     const form = page.locator('[data-testid="content-report-popover"]')
 
-    expect(await form.screenshot({ timeout: 100 })).toMatchSnapshot({
+    expect(await form.screenshot({ timeout: 500 })).toMatchSnapshot({
       name: 'content-report-unfocused.png',
     })
   })
 
   test('focused close button', async ({ page }) => {
-    await page.click('button:has-text("report")', { timeout: 100 })
+    await page.click('button:has-text("report")', { timeout: 500 })
     const form = page.locator('[data-testid="content-report-popover"]')
 
-    await form.locator('[aria-label="Close"]').focus({ timeout: 100 })
+    await form.locator('[aria-label="Close"]').focus({ timeout: 500 })
     expect(await form.screenshot()).toMatchSnapshot({
       name: 'content-report-focused.png',
     })
