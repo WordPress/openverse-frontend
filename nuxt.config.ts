14cb02c3a502a9fd942f0208abacbab221c94e30
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-04-26 16:04:16 +0200

    Use pure bundle names in CI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -108,6 +108,26 @@ const head = {
   ],
 }
 
+const baseProdName = process.env.CI ? '[name]' : '[contenthash:7]'
+
+const filenames: NonNullable<NuxtConfig['build']>['filenames'] = {
+  app: ({ isDev, isModern }) =>
+    isDev
+      ? `[name]${isModern ? '.modern' : ''}.js`
+      : `${baseProdName}${isModern ? '.modern' : ''}.js`,
+  chunk: ({ isDev, isModern }) =>
+    isDev
+      ? `[name]${isModern ? '.modern' : ''}.js`
+      : `${baseProdName}${isModern ? '.modern' : ''}.js`,
+  css: ({ isDev }) => (isDev ? '[name].css' : `css/${baseProdName}.css`),
+  img: ({ isDev }) =>
+    isDev ? '[path][name].[ext]' : `img/${baseProdName}.[ext]`,
+  font: ({ isDev }) =>
+    isDev ? '[path][name].[ext]' : `fonts/${baseProdName}.[ext]`,
+  video: ({ isDev }) =>
+    isDev ? '[path][name].[ext]' : `videos/${baseProdName}.[ext]`,
+}
+
 const config: NuxtConfig = {
   // eslint-disable-next-line no-undef
   version: pkg.version, // used to purge cache :)
@@ -225,24 +245,7 @@ const config: NuxtConfig = {
         dst: 'index.js',
       },
     ],
-    filenames: {
-      app: ({ isDev, isModern }) =>
-        isDev
-          ? `[name]${isModern ? '.modern' : ''}.js`
-          : `[name].[contenthash:7]${isModern ? '.modern' : ''}.js`,
-      chunk: ({ isDev, isModern }) =>
-        isDev
-          ? `[name]${isModern ? '.modern' : ''}.js`
-          : `[name].[contenthash:7]${isModern ? '.modern' : ''}.js`,
-      css: ({ isDev }) =>
-        isDev ? '[name].css' : 'css/[name].[contenthash:7].css',
-      img: ({ isDev }) =>
-        isDev ? '[path][name].[ext]' : 'img/[name].[contenthash:7].[ext]',
-      font: ({ isDev }) =>
-        isDev ? '[path][name].[ext]' : 'fonts/[name].[contenthash:7].[ext]',
-      video: ({ isDev }) =>
-        isDev ? '[path][name].[ext]' : 'videos/[name].[contenthash:7].[ext]',
-    },
+    filenames,
     friendlyErrors: false,
     postcss: {
       plugins: {
