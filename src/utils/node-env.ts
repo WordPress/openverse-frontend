49e280037158e58d9f08bea0207868780a215d33
Dhruv Bhanushali <dhruv_b@live.com>
2022-05-03 09:09:33 +0400

    Separate `NODE_ENV` from `DEPLOYMENT_ENV`

--- src/utils/node-env.ts
+++ src/utils/node-env.ts
@@ -1,5 +1,3 @@
-import { DEVELOPMENT, TEST, PRODUCTION } from '../constants/node-env'
-
-export const isDev: boolean = process.env.NODE_ENV === DEVELOPMENT
-export const isTest: boolean = process.env.NODE_ENV === TEST
-export const isProd: boolean = process.env.NODE_ENV === PRODUCTION
+export const isDev: boolean = process.env.NODE_ENV === 'development'
+export const isTest: boolean = process.env.NODE_ENV === 'test'
+export const isProd: boolean = process.env.NODE_ENV === 'production'
