b57e63d983f770d1860eda031377a1d4a1ca772f
Olga Bulat <obulat@gmail.com>
2022-06-17 11:19:14 +0300

    Correctly usu dirs in media-reuse tests

--- test/playwright/visual-regression/components/media-reuse.spec.ts
+++ test/playwright/visual-regression/components/media-reuse.spec.ts
@@ -13,26 +13,28 @@ const tabs = [
 ]
 test.describe('media-reuse', () => {
   for (const dir of renderDirs) {
-    test.describe(dir, () => {
-      test.beforeEach(async ({ page }) => {
-        await page.goto('/image/f9384235-b72e-4f1e-9b05-e1b116262a29')
-        await dismissTranslationBanner(page)
-      })
-      for (const tab of tabs) {
-        breakpoints.describeEvery(({ expectSnapshot }) => {
-          test(`Should render media reuse section with "${tab.name}" tab open on ${dir}`, async ({
-            page,
-          }) => {
-            await page.locator(`text=${tab.name}`).click()
-            // Make sure the tab is not focused and doesn't have a pink ring
-            await page.locator('h3:has-text("Reuse content")').click()
-            await expectSnapshot(
-              `media-reuse-${dir}-${tab.id}-tab`,
-              page.locator('.media-reuse')
-            )
-          })
-        })
-      }
+    test.beforeEach(async ({ page }) => {
+      await page.goto(
+        `${
+          dir === 'ltr' ? '' : '/ar'
+        }/image/f9384235-b72e-4f1e-9b05-e1b116262a29`
+      )
+      await dismissTranslationBanner(page)
     })
+    for (const tab of tabs) {
+      breakpoints.describeEvery(({ expectSnapshot }) => {
+        test(`Should render media reuse section with "${tab.name}" tab open on ${dir}`, async ({
+          page,
+        }) => {
+          await page.locator(`text=${tab.name}`).click()
+          // Make sure the tab is not focused and doesn't have a pink ring
+          await page.locator('h3:has-text("Reuse content")').click()
+          await expectSnapshot(
+            `media-reuse-${dir}-${tab.id}-tab`,
+            page.locator('.media-reuse')
+          )
+        })
+      })
+    }
   }
 })
