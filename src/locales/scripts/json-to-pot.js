de273480ad89492a53a75822fdb9a3c6e74247f2
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2022-06-29 13:57:44 +1000

    Fix ENOENT issue due to nonexistent directory

--- src/locales/scripts/json-to-pot.js
+++ src/locales/scripts/json-to-pot.js
@@ -1,13 +1,14 @@
 const fs = require('fs')
+const path = require('path')
 
 const json = require('../en.json')
 
 const { createPotFile } = require('./json-pot-helpers')
 
 try {
-  const fileName = process.cwd() + '/src/locales/po-files/openverse.pot'
+  const fileName = path.resolve(process.cwd(), 'openverse.pot')
 
-  fs.writeFileSync(fileName, createPotFile(json))
+  fs.writeFileSync(fileName, createPotFile(json), { flag: 'w' })
   console.log(`Successfully wrote pot file to ${fileName}`)
 } catch (err) {
   console.error(err)
