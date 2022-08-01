1ae858e766a05d542817d468995c53dd245f84db
Dhruv Bhanushali <dhruv_b@live.com>
2022-08-01 13:51:43 +0530

    Update `timeFmt` to support durations longer than 24h

--- src/utils/time-fmt.ts
+++ src/utils/time-fmt.ts
@@ -4,7 +4,15 @@
  * @returns the duration in a human-friendly format
  */
 export const timeFmt = (seconds: number): string => {
-  const date = new Date(0)
-  date.setSeconds(Number.isFinite(seconds) ? seconds : 0)
-  return date.toISOString().substr(11, 8).replace(/^00:/, '')
+  const hrs = ~~(seconds / 3600)
+  let mins = (~~((seconds % 3600) / 60)).toString()
+  const secs = (~~seconds % 60).toString().padStart(2, '0') // always padded
+
+  const parts = []
+  if (hrs > 0) {
+    parts.push(hrs)
+    mins = mins.padStart(2, '0') // padded only if hours present
+  }
+  parts.push(mins, secs)
+  return parts.join(':')
 }
