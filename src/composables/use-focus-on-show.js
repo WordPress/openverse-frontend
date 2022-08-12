df4413423452db8771895e8bb74392b9f145a29d
Olga Bulat <obulat@gmail.com>
2022-08-12 10:22:15 +0300

    Revert conversion to TS

--- src/composables/use-focus-on-show.js
+++ src/composables/use-focus-on-show.js
@@ -1,4 +1,4 @@
-import { ref, watch, Ref } from '@nuxtjs/composition-api'
+import { ref, watch } from '@nuxtjs/composition-api'
 
 import { warn } from '~/utils/console'
 import {
@@ -10,49 +10,49 @@ import {
 export const noFocusableElementWarning =
   "It's recommended to have at least one tabbable element inside dialog. The dialog element has been automatically focused. If this is the intended behavior, pass `tabIndex={0}` to the dialog element to disable this warning."
 
-type Props = {
-  dialogRef: Ref<HTMLElement>
-  visibleRef: Ref<boolean>
-  autoFocusOnShowRef: Ref<boolean>
-  initialFocusElementRef: Ref<HTMLElement | undefined>
-}
+/**
+ * @typedef Props
+ * @property {import('./types').Ref<HTMLElement>} dialogRef
+ * @property {import('./types').Ref<boolean>} visibleRef
+ * @property {import('./types').Ref<boolean>} autoFocusOnShowRef
+ * @property {import('./types').Ref<HTMLElement>} initialFocusElementRef
+ */
+
 /**
  * @see https://github.com/reakit/reakit/blob/bce9b8a0e567983f61b5cc627f8dee9461986fab/packages/reakit/src/Dialog/__utils/useFocusOnShow.ts#L9
+ * @param {Props} props
  */
 export const useFocusOnShow = ({
   dialogRef,
   visibleRef,
   autoFocusOnShowRef,
   initialFocusElementRef = ref(),
-}: Props) => {
+}) => {
   watch(
-    [
-      dialogRef,
-      visibleRef,
-      autoFocusOnShowRef,
-      initialFocusElementRef,
-    ] as const,
-
+    [dialogRef, visibleRef, autoFocusOnShowRef, initialFocusElementRef],
+    /**
+     * @param {[HTMLElement, boolean, boolean, HTMLElement]} values
+     */
     ([dialog, visible, autoFocusOnShow, initialFocusElement]) => {
       if (!dialog || !visible || !autoFocusOnShow) return
 
       const isActive = () => hasFocusWithin(dialog)
 
       if (initialFocusElement) {
-        ensureFocus(initialFocusElement, {
+        return ensureFocus(initialFocusElement, {
           preventScroll: true,
           isActive,
         })
-      } else {
-        const tabbable = getFirstTabbableIn(dialog, true)
+      }
 
-        if (tabbable) {
-          ensureFocus(tabbable, { preventScroll: true, isActive })
-        } else {
-          ensureFocus(dialog, { preventScroll: true, isActive })
-          if (dialog.tabIndex === undefined || dialog.tabIndex < 0) {
-            warn(noFocusableElementWarning)
-          }
+      const tabbable = getFirstTabbableIn(dialog, true)
+
+      if (tabbable) {
+        ensureFocus(tabbable, { preventScroll: true, isActive })
+      } else {
+        ensureFocus(dialog, { preventScroll: true, isActive })
+        if (dialog.tabIndex === undefined || dialog.tabIndex < 0) {
+          warn(noFocusableElementWarning)
         }
       }
     }
