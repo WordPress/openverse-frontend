b90bf5ce73c9b431e8dec0e118dea75b795f4c4a
Olga Bulat <obulat@gmail.com>
2022-08-23 16:33:07 +0300

    Update snapshots

--- test/playwright/visual-regression/components/external-sources-section.spec.ts
+++ test/playwright/visual-regression/components/external-sources-section.spec.ts
@@ -11,31 +11,20 @@ test.describe.configure({ mode: 'parallel' })
 
 for (const dir of languageDirections) {
   for (const searchType of supportedSearchTypes) {
-    test(`additional ${searchType} sources section - ${dir}`, async ({
+    test(`External ${searchType} sources popover - ${dir}`, async ({
       page,
     }) => {
       await goToSearchTerm(page, 'birds', { searchType, dir })
-      const additionalSourcesSection = page.locator('.meta-search')
-
-      expect(await additionalSourcesSection.screenshot()).toMatchSnapshot({
-        name: `additional-sources-section-${dir}.png`,
-      })
-    })
-
-    test(`additional ${searchType} sources popover - ${dir}`, async ({
-      page,
-    }) => {
-      await goToSearchTerm(page, 'birds', { searchType, dir })
-      const additionalSourcesButton = page.locator(
+      const externalSourcesButton = page.locator(
         '[aria-controls="source-list-popover"]'
       )
 
-      await additionalSourcesButton.click()
+      await externalSourcesButton.click()
 
       expect(
         await page.locator('[data-testid="source-list-popover"]').screenshot()
       ).toMatchSnapshot({
-        name: `additional-${searchType}-sources-popover-${dir}.png`,
+        name: `external-${searchType}-sources-popover-${dir}.png`,
       })
     })
   }
