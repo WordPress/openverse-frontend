33fcbe5f29954c995725c32b140de5e9694ca60c
Olga Bulat <obulat@gmail.com>
2022-08-23 13:26:10 +0300

    Update snapshots

--- test/playwright/visual-regression/components/additional-sources-section.spec.ts
+++ test/playwright/visual-regression/components/additional-sources-section.spec.ts
@@ -9,36 +9,34 @@ import { supportedSearchTypes } from '~/constants/media'
 
 test.describe.configure({ mode: 'parallel' })
 
-test.describe('additional sources section', () => {
-  for (const dir of languageDirections) {
-    for (const searchType of supportedSearchTypes) {
-      test(`additional ${searchType} sources section - ${dir}`, async ({
-        page,
-      }) => {
-        await goToSearchTerm(page, 'birds', { searchType, dir })
-        const additionalSourcesSection = page.locator('.meta-search')
+for (const dir of languageDirections) {
+  for (const searchType of supportedSearchTypes) {
+    test(`additional ${searchType} sources section - ${dir}`, async ({
+      page,
+    }) => {
+      await goToSearchTerm(page, 'birds', { searchType, dir })
+      const additionalSourcesSection = page.locator('.meta-search')
 
-        expect(await additionalSourcesSection.screenshot()).toMatchSnapshot({
-          name: `additional-sources-section-${dir}.png`,
-        })
+      expect(await additionalSourcesSection.screenshot()).toMatchSnapshot({
+        name: `additional-sources-section-${dir}.png`,
       })
+    })
 
-      test(`additional ${searchType} sources popover - ${dir}`, async ({
-        page,
-      }) => {
-        await goToSearchTerm(page, 'birds', { searchType, dir })
-        const additionalSourcesButton = page.locator(
-          '[aria-controls="source-list-popover"]'
-        )
+    test(`additional ${searchType} sources popover - ${dir}`, async ({
+      page,
+    }) => {
+      await goToSearchTerm(page, 'birds', { searchType, dir })
+      const additionalSourcesButton = page.locator(
+        '[aria-controls="source-list-popover"]'
+      )
 
-        await additionalSourcesButton.click()
+      await additionalSourcesButton.click()
 
-        expect(
-          await page.locator('[data-testid="source-list-popover"]').screenshot()
-        ).toMatchSnapshot({
-          name: `additional-${searchType}-sources-popover-${dir}.png`,
-        })
+      expect(
+        await page.locator('[data-testid="source-list-popover"]').screenshot()
+      ).toMatchSnapshot({
+        name: `additional-${searchType}-sources-popover-${dir}.png`,
       })
-    }
+    })
   }
-})
+}
