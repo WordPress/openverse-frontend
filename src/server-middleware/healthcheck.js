8eef1b35eeb02fa3a3156f4cfa63c49fe84e6123
Zack Krida <zackkrida@pm.me>
2022-08-25 17:24:24 -0400

    Add healthcheck which considers desired avaliable memory percentage
    Adds a healthcheck which compares avaliable system memory to our desired amount of avaliable memory. This allows us to fail the healthcheck *before* the app is actually unavaliable, avoiding downtime

--- src/server-middleware/healthcheck.js
+++ src/server-middleware/healthcheck.js
@@ -1,4 +1,52 @@
-export default function healthcheck(req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('OK')
+import osu from 'node-os-utils'
+
+/**
+ * A Healthcheck which passes if the amount of avaliable memory
+ * is greater than our desired minimum avaliable memory percentage.
+ *
+ * Useful in the event of memory leaks, in particular.
+ *
+ * @type {import('@nuxt/types').ServerMiddleware}
+ */
+export default function Healthcheck(_, res) {
+  osu.mem
+    .info()
+    .then((info) => {
+      const avaliableMemory = info.freeMemPercentage
+
+      /**
+       * Server Middleware do not have access to any context or configuration,
+       * so we use a raw environment variable and set a default here, instead
+       * of in `nuxt.config.ts` or `utils/env.ts`.
+       */
+      const desiredAvaliableMemory = Number(
+        process.env.HEALTHCHECK_DESIRED_AVALIABLE_MEMORY_PERCENTAGE ?? '10'
+      )
+
+      const avaliableMemoryIsSufficient =
+        avaliableMemory > desiredAvaliableMemory
+
+      // Log memory information in non-production contexts
+      if (process.env.NODE_ENV !== 'production') {
+        console.info(`Healthcheck memory information: `, {
+          avaliableMemory,
+          desiredAvaliableMemory,
+          avaliableMemoryIsSufficient,
+        })
+      }
+
+      if (!avaliableMemoryIsSufficient) {
+        throw Error(
+          `Avaliable memory percentage ${avaliableMemory} is below desired ${desiredAvaliableMemory}`
+        )
+      }
+
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('OK')
+    })
+    .catch((error) => {
+      console.error(error)
+      res.statusCode = 503
+      res.end('Healthcheck Failed')
+    })
 }
