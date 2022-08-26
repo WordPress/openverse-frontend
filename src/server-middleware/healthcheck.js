f38d89d484be34448ccde9cf5b7dfa7185fd7cca
Zack Krida <zackkrida@pm.me>
2022-08-26 14:44:52 -0400

    Switch from promise chain to async/await; switch default value to 20

--- src/server-middleware/healthcheck.js
+++ src/server-middleware/healthcheck.js
@@ -1,4 +1,4 @@
-import osu from 'node-os-utils'
+const osu = require('node-os-utils')
 
 /**
  * A Healthcheck which passes if the amount of avaliable memory
@@ -8,42 +8,40 @@ import osu from 'node-os-utils'
  *
  * @type {import('@nuxt/types').ServerMiddleware}
  */
-export default function Healthcheck(_, res) {
-  osu.mem
-    .info()
-    .then((info) => {
-      const avaliableMemory = info.freeMemPercentage
+export default async function Healthcheck(_, res) {
+  try {
+    const info = await osu.mem.info()
+    const avaliableMemory = info.freeMemPercentage
 
-      /**
-       * Server Middleware do not have access to any context or configuration,
-       * so we use a raw environment variable and set a default here, instead
-       * of in `nuxt.config.ts` or `utils/env.ts`.
-       */
-      const desiredAvaliableMemory = Number(
-        process.env.HEALTHCHECK_DESIRED_AVALIABLE_MEMORY_PERCENTAGE ?? '15'
+    /**
+     * Server Middleware do not have access to any context or configuration,
+     * so we use a raw environment variable and set a default here, instead
+     * of in `nuxt.config.ts` or `utils/env.ts`.
+     */
+    const desiredAvaliableMemory = Number(
+      process.env.HEALTHCHECK_DESIRED_AVALIABLE_MEMORY_PERCENTAGE ?? '20'
+    )
+
+    const avaliableMemoryIsSufficient = avaliableMemory > desiredAvaliableMemory
+
+    console.info(`Healthcheck memory information: `, {
+      avaliableMemory,
+      desiredAvaliableMemory,
+      avaliableMemoryIsSufficient,
+    })
+
+    if (!avaliableMemoryIsSufficient) {
+      throw Error(
+        `Avaliable memory percentage ${avaliableMemory} is below desired ${desiredAvaliableMemory}`
       )
+    }
 
-      const avaliableMemoryIsSufficient =
-        avaliableMemory > desiredAvaliableMemory
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('OK')
+  } catch (error) {
+    console.error(error)
 
-      console.info(`Healthcheck memory information: `, {
-        avaliableMemory,
-        desiredAvaliableMemory,
-        avaliableMemoryIsSufficient,
-      })
-
-      if (!avaliableMemoryIsSufficient) {
-        throw Error(
-          `Avaliable memory percentage ${avaliableMemory} is below desired ${desiredAvaliableMemory}`
-        )
-      }
-
-      res.setHeader('Content-Type', 'text/plain')
-      res.end('OK')
-    })
-    .catch((error) => {
-      console.error(error)
-      res.statusCode = 503
-      res.end('Healthcheck Failed')
-    })
+    res.statusCode = 503
+    res.end('Healthcheck Failed')
+  }
 }
