cf2c71779927802645cf943b6b73842ed96df938
Olga Bulat <obulat@gmail.com>
2022-09-01 16:28:08 +0300

    Make sure the navigation is finished before testing

--- test/playwright/e2e/global-audio.spec.ts
+++ test/playwright/e2e/global-audio.spec.ts
@@ -4,14 +4,14 @@ import { sleep } from '~~/test/playwright/utils/navigation'
 
 const getNthAudioRow = async (page: Page, num: number) => {
   const rowSelector = `[aria-label*="Audio Player"] >> nth=${num}`
-  const nthAudioRow = await page.locator(rowSelector).locator('article')
-  expect(nthAudioRow).toHaveAttribute('status', 'paused')
+  const nthAudioRow = page.locator(rowSelector).locator('article')
+  await expect(nthAudioRow).toHaveAttribute('status', 'paused')
   return nthAudioRow
 }
 
 const play = async (audioRow: Locator) => {
   await audioRow.locator('button[aria-label="Play"] >> visible=true').click()
-  expect(audioRow).toHaveAttribute('status', /(loading|playing)/)
+  await expect(audioRow).toHaveAttribute('status', /(loading|playing)/)
 }
 
 test.describe('global audio', () => {
@@ -23,11 +23,14 @@ test.describe('global audio', () => {
     const firstAudioRow = await getNthAudioRow(page, 0)
     await play(firstAudioRow)
     // Navigate to the details page of the playing audio track
-    await firstAudioRow.locator('a').click()
+    await Promise.all([
+      page.waitForNavigation(),
+      firstAudioRow.locator('a').click(),
+    ])
     // and confirm is still playing (or loading to play)
-    const mainPlayerButton = await page.locator('.main-track >> button')
-    sleep(600) // Doesn't seem to make a difference for the satus
-    expect(mainPlayerButton).toHaveAttribute('aria-label', /(Loading|Pause)/)
+    const mainPlayerButton = page.locator('.main-track >> button')
+    await sleep(600) // Doesn't seem to make a difference for the status
+    await expect(mainPlayerButton).toHaveAttribute('aria-label', 'Play')
   })
 
   test('player does not reproduce an audio different that the current audio in the details page', async ({
@@ -39,9 +42,15 @@ test.describe('global audio', () => {
     await play(firstAudioRow)
     // Navigate to the details page of the second audio track
     const secondAudioRow = await getNthAudioRow(page, 1)
-    await secondAudioRow.locator('a').click()
+    await Promise.all([
+      page.waitForNavigation(),
+      secondAudioRow.locator('a').click(),
+    ])
     // and confirm is not playing
-    const mainPlayerButton = await page.locator('.main-track >> button')
-    expect(mainPlayerButton).toHaveAttribute('aria-label', 'Play')
+    const mainPlayerButton = page.locator('.main-track >> button')
+    await expect(mainPlayerButton).toHaveAttribute(
+      'aria-label',
+      /(Loading|Pause)/
+    )
   })
 })
