19d82c79f133f45a1ddb55ea004f15f66ea67fbe
Olga Bulat <obulat@gmail.com>
2022-09-01 21:35:24 +0300

    Fix global-audio tests

--- test/playwright/e2e/global-audio.spec.ts
+++ test/playwright/e2e/global-audio.spec.ts
@@ -4,8 +4,8 @@ import { sleep } from '~~/test/playwright/utils/navigation'
 
 const getNthAudioRow = async (page: Page, num: number) => {
   const rowSelector = `[aria-label*="Audio Player"] >> nth=${num}`
-  const nthAudioRow = page.locator(rowSelector).locator('article')
-  await expect(nthAudioRow).toHaveAttribute('status', 'paused')
+  const nthAudioRow = await page.locator(rowSelector).locator('article')
+  expect(nthAudioRow).toHaveAttribute('status', 'paused')
   return nthAudioRow
 }
 
@@ -23,15 +23,14 @@ test.describe('global audio', () => {
     const firstAudioRow = await getNthAudioRow(page, 0)
     await play(firstAudioRow)
     // Navigate to the details page of the playing audio track
-    await Promise.all([
-      page.waitForNavigation(),
-      firstAudioRow.locator('a').click(),
-    ])
+    await firstAudioRow.locator('a').click()
     // and confirm is still playing (or loading to play)
-    const mainPlayerButton = page.locator('.main-track >> button')
+    const mainPlayerButton = await page.locator('.main-track >> button')
     await sleep(600) // Doesn't seem to make a difference for the status
-    // When the audio is playing, the button's aria-label is 'Pause'
-    await expect(mainPlayerButton).toHaveAttribute('aria-label', 'Pause')
+    await expect(mainPlayerButton).toHaveAttribute(
+      'aria-label',
+      /(Loading|Pause)/
+    )
   })
 
   test('player does not reproduce an audio different that the current audio in the details page', async ({
@@ -43,13 +42,9 @@ test.describe('global audio', () => {
     await play(firstAudioRow)
     // Navigate to the details page of the second audio track
     const secondAudioRow = await getNthAudioRow(page, 1)
-    await Promise.all([
-      page.waitForNavigation(),
-      secondAudioRow.locator('a').click(),
-    ])
+    await secondAudioRow.locator('a').click()
     // and confirm is not playing
     const mainPlayerButton = page.locator('.main-track >> button')
-    // When the audio is playing, the button's aria-label is 'Play'
     await expect(mainPlayerButton).toHaveAttribute('aria-label', 'Play')
   })
 })
