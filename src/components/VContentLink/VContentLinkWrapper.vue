d666490757a8438694325ca0c0f099b85c0c4a75
Olga Bulat <obulat@gmail.com>
2022-09-20 22:34:01 +0300

    Simplify classes

--- src/components/VContentLink/VContentLinkWrapper.vue
+++ src/components/VContentLink/VContentLinkWrapper.vue
@@ -4,25 +4,14 @@
     v-if="hasResults"
     :href="to"
     role="link"
-    :aria-disabled="!hasResults"
-    class="flex w-full flex-col items-start overflow-hidden rounded-sm border border-dark-charcoal/20 bg-white py-4 ps-4 pe-12 md:flex-row md:items-center md:justify-between md:p-6"
-    :class="
-      hasResults
-        ? ' text-dark-charcoal hover:bg-dark-charcoal hover:text-white hover:no-underline focus:border-tx focus:outline-none focus-visible:ring focus-visible:ring-pink'
-        : 'cursor-not-allowed text-dark-charcoal/40'
-    "
+    class="flex w-full flex-col items-start overflow-hidden rounded-sm border border-dark-charcoal/20 bg-white py-4 text-dark-charcoal ps-4 pe-12 hover:bg-dark-charcoal hover:text-white hover:no-underline focus:border-tx focus:outline-none focus-visible:ring focus-visible:ring-pink md:flex-row md:items-center md:justify-between md:p-6"
     @keydown.native.shift.tab.exact="$emit('shift-tab', $event)"
   >
     <slot />
   </VLink>
   <div
     v-else
-    class="flex w-full flex-col items-start overflow-hidden rounded-sm border border-dark-charcoal/20 bg-white py-4 ps-4 pe-12 md:flex-row md:items-center md:justify-between md:p-6"
-    :class="
-      hasResults
-        ? 'text-dark-charcoal hover:bg-dark-charcoal hover:text-white hover:no-underline focus:border-tx focus:outline-none focus-visible:ring focus-visible:ring-pink'
-        : 'cursor-not-allowed text-dark-charcoal/40'
-    "
+    class="flex w-full cursor-not-allowed flex-col items-start overflow-hidden rounded-sm border border-dark-charcoal/20 bg-white py-4 text-dark-charcoal/40 ps-4 pe-12 md:flex-row md:items-center md:justify-between md:p-6"
   >
     <slot />
   </div>
