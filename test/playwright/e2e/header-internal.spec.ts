51c1ed262c28f865b52da018c205261b3c6a60f6
Olga Bulat <obulat@gmail.com>
2022-09-22 08:55:10 +0300

    Add a test for external links in the modal

--- test/playwright/e2e/header-internal.spec.ts
+++ test/playwright/e2e/header-internal.spec.ts
@@ -10,7 +10,7 @@ test.use({
   viewport: { width: 640, height: 600 },
 })
 
-test('can open and close the modal on md breakpoint', async ({ page }) => {
+test.skip('can open and close the modal on md breakpoint', async ({ page }) => {
   await enableNewHeader(page)
 
   await page.goto('/about')
@@ -29,7 +29,7 @@ test('can open and close the modal on md breakpoint', async ({ page }) => {
   await expect(page.locator(`[aria-label="${menuAriaLabel}"]`)).toBeVisible()
 })
 
-test('the modal locks the scroll on md breakpoint', async ({ page }) => {
+test.skip('the modal locks the scroll on md breakpoint', async ({ page }) => {
   await enableNewHeader(page)
 
   await page.goto('/about')
@@ -41,4 +41,19 @@ test('the modal locks the scroll on md breakpoint', async ({ page }) => {
 
   const scrollPosition = await page.evaluate(() => window.scrollY)
   expect(scrollPosition).toBeGreaterThan(100)
+})
+
+test("the modal opens an external link in a new window and it doesn't close the modal", async ({
+  page,
+}) => {
+  await enableNewHeader(page)
+
+  await page.goto('/about')
+  const menuAriaLabel = t('header.aria.menu')
+
+  await scrollToBottom(page)
+  await page.locator(`[aria-label="${menuAriaLabel}"]`).click()
+  await page.locator('div[role="dialog"] >> text=API').click()
+
+  await expect(page.locator('[role="dialog"]')).toBeVisible()
 })
