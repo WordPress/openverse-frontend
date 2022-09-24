32f9515ac0fb538089dfc8276e0f078b78144537
Olga Bulat <obulat@gmail.com>
2022-09-24 10:01:14 +0300

    Add VSearchTypeButton.vue Storybook snapshots

--- test/storybook/visual-regression/v-search-type-button.spec.ts
+++ test/storybook/visual-regression/v-search-type-button.spec.ts
@@ -1,81 +1,82 @@
 import { test } from '@playwright/test'
 
 import breakpoints from '~~/test/playwright/utils/breakpoints'
-import { makeGotoWithArgs } from '~~/test/storybook/utils/args'
 
-const gotoWithArgs = makeGotoWithArgs(
-  'components-vheader-vsearchtypebutton--default-story'
-)
+const defaultUrl =
+  '/iframe.html?id=components-vheader-vsearchtypebutton--default-story'
+const pressedUrl = `${defaultUrl}&args=a11yProps.aria-pressed:true;a11yProps.aria-haspopup:dialog`
+
+const searchTypeButtonLocator = 'button[aria-haspopup="dialog"]'
 
 test.describe('VSearchTypeButton', () => {
   breakpoints.describeLg(({ expectSnapshot }) => {
     test('resting', async ({ page }) => {
-      await gotoWithArgs(page)
+      await page.goto(defaultUrl)
       await expectSnapshot(
         'v-search-type-button-at-rest',
-        page.locator('button')
+        page.locator(searchTypeButtonLocator)
       )
     })
 
     test('hovered', async ({ page }) => {
-      await gotoWithArgs(page)
-      await page.hover('button')
+      await page.goto(defaultUrl)
+      await page.hover(searchTypeButtonLocator)
       await expectSnapshot(
         'v-search-type-button-hovered',
-        page.locator('button')
+        page.locator(searchTypeButtonLocator)
       )
     })
 
     test('pressed', async ({ page }) => {
-      await gotoWithArgs(page, { a11yProps: { 'aria-pressed': true } })
+      await page.goto(pressedUrl)
       await expectSnapshot(
-        'v-search-type-button-hovered',
-        page.locator('button')
+        'v-search-type-button-pressed',
+        page.locator(searchTypeButtonLocator)
       )
     })
 
     test('pressed-hovered', async ({ page }) => {
-      await gotoWithArgs(page, { a11yProps: { 'aria-pressed': true } })
-      await page.hover('button')
+      await page.goto(pressedUrl)
+      await page.hover(searchTypeButtonLocator)
       await expectSnapshot(
-        'v-search-type-button-hovered',
-        page.locator('button')
+        'v-search-type-button-pressed-hovered',
+        page.locator(searchTypeButtonLocator)
       )
     })
   })
 
   breakpoints.describeXl(({ expectSnapshot }) => {
     test('resting', async ({ page }) => {
-      await gotoWithArgs(page)
+      await page.goto(defaultUrl)
       await expectSnapshot(
-        'v-search-type-button-xl-at-rest',
-        page.locator('button')
+        'v-search-type-button-with-text-at-rest',
+        page.locator(searchTypeButtonLocator)
       )
     })
 
     test('hovered', async ({ page }) => {
-      await gotoWithArgs(page)
-      await page.hover('button')
+      await page.goto(defaultUrl)
+      await page.hover(searchTypeButtonLocator)
       await expectSnapshot(
-        'v-search-type-button-xl-hovered',
-        page.locator('button')
+        'v-search-type-button-with-text-hovered',
+        page.locator(searchTypeButtonLocator)
       )
     })
 
     test('pressed', async ({ page }) => {
-      await gotoWithArgs(page, { a11yProps: { 'aria-pressed': true } })
+      await page.goto(pressedUrl)
       await expectSnapshot(
-        'v-search-type-button-xl-hovered',
-        page.locator('button')
+        'v-search-type-button-with-text-pressed',
+        page.locator(searchTypeButtonLocator)
       )
     })
 
     test('pressed-hovered', async ({ page }) => {
-      await gotoWithArgs(page, { a11yProps: { 'aria-pressed': true } })
-      await page.hover('button')
+      await page.goto(pressedUrl)
+      await page.hover(searchTypeButtonLocator)
       await expectSnapshot(
-        'v-search-type-button-xl-hovered',
-        page.locator('button')
+        'v-search-type-button-with-text-pressed-hovered',
+        page.locator(searchTypeButtonLocator)
       )
     })
   })
