11029183805fb97d78773187707a248b59d08092
Olga Bulat <obulat@gmail.com>
2022-09-26 18:28:57 +0300

    Update tests

--- test/storybook/visual-regression/v-button.spec.ts
+++ test/storybook/visual-regression/v-button.spec.ts
@@ -6,26 +6,22 @@ import { buttonVariants } from '~/types/button'
 
 const buttonLocator = 'text=Code is Poetry'
 
+test.describe.configure({ mode: 'parallel' })
+
 test.describe('VButton', () => {
   const gotoWithArgs = makeGotoWithArgs('components-vbutton--v-button')
   const nonPressedVariants = buttonVariants.filter(
     (name) => !name.endsWith('pressed')
   )
   for (const variant of nonPressedVariants) {
-    test(`resting ${variant}`, async ({ page }) => {
+    test(`${variant} resting`, async ({ page }) => {
       await gotoWithArgs(page, { variant })
       expect(await page.locator(buttonLocator).screenshot()).toMatchSnapshot({
         name: `${variant}-resting.png`,
       })
     })
-    test(`focused ${variant}`, async ({ page }) => {
-      await gotoWithArgs(page, { variant })
-      await page.focus(buttonLocator)
-      expect(await page.locator(buttonLocator).screenshot()).toMatchSnapshot({
-        name: `${variant}-hovered.png`,
-      })
-    })
-    test(`hovered ${variant}`, async ({ page }) => {
+
+    test(`${variant} hovered`, async ({ page }) => {
       await gotoWithArgs(page, { variant })
       await page.hover(buttonLocator)
       expect(await page.locator(buttonLocator).screenshot()).toMatchSnapshot({
@@ -33,26 +29,18 @@ test.describe('VButton', () => {
       })
     })
 
-    test(`pressed ${variant}`, async ({ page }) => {
+    test(`${variant} pressed`, async ({ page }) => {
       await gotoWithArgs(page, { variant, pressed: true })
       expect(await page.locator(buttonLocator).screenshot()).toMatchSnapshot({
         name: `${variant}-pressed.png`,
       })
     })
 
-    test(`pressed hovered ${variant}`, async ({ page }) => {
+    test(`${variant} pressed hovered`, async ({ page }) => {
       await gotoWithArgs(page, { variant })
       await page.hover(buttonLocator)
       expect(await page.locator(buttonLocator).screenshot()).toMatchSnapshot({
-        name: `${variant}-hovered.png`,
-      })
-    })
-
-    test(`pressed focused ${variant}`, async ({ page }) => {
-      await gotoWithArgs(page, { variant })
-      await page.focus(buttonLocator)
-      expect(await page.locator(buttonLocator).screenshot()).toMatchSnapshot({
-        name: `${variant}-hovered.png`,
+        name: `${variant}-pressed-hovered.png`,
       })
     })
   }
