2a632ea2abe525e48bd5df0bfb9c86fb0944bb76
Olga Bulat <obulat@gmail.com>
2022-10-03 13:13:36 +0300

    Implement the composite search bar in the mobile search header

---
+++ src/components/VHeader/VHeaderMobile/VBackButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <VButton
+    size="disabled"
+    class="icon-button flex h-12 w-12 flex-shrink-0 rotate-90 items-center justify-center bg-tx active:shadow-ring"
+    variant="plain--avoid"
+    :aria-label="$t('header.back-button')"
+    @click="$emit('click')"
+  >
+    <span
+      class="flex h-8 w-8 items-center justify-center rounded-sm bg-dark-charcoal-10"
+    >
+      <VIcon
+        class="bg-red pointer-events-none rounded-sm bg-dark-charcoal-10"
+        :icon-path="caretDownIcon"
+      />
+    </span>
+  </VButton>
+</template>
+
+<script lang="ts">
+import { defineEvent } from '~/types/emits'
+
+import VButton from '~/components/VButton.vue'
+import VIcon from '~/components/VIcon/VIcon.vue'
+
+import caretDownIcon from '~/assets/icons/caret-down.svg'
+
+export default {
+  name: 'VBackButton',
+  components: { VButton, VIcon },
+  emits: {
+    click: defineEvent(),
+  },
+  setup() {
+    return {
+      caretDownIcon,
+    }
+  },
+}
+</script>
