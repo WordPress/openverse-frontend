d4e3bdf8e510dba22bda33dca0cf0ba6871a2b24
symbiosisafrica <a.kluge202@gmail.com>
2022-11-13 16:24:39 +0200

    add remake folder, component template file and stories template file

---
+++ .remake/component/remake-name/remake-name.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: '<%= name %>',
+})
+</script>
+
+<style scoped></style>
