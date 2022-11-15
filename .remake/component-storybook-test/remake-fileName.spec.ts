11c6ecb793dd4c906f04e2ca5a8bc91d53b6c627
anton202 <a.kluge202@gmail.com>
2022-11-15 13:50:45 +0200

    changed file name of component story book test template

--- .remake/component-storybook-test/remake-fileName.spec.ts
+++ .remake/component-storybook-test/remake-fileName.spec.ts
@@ -4,7 +4,9 @@ import breakpoints from '~~/test/playwright/utils/breakpoints'
 
 test.describe('<%= name %>', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('/iframe.html?id=components-<%= name %>--default-story')
+    await page.goto(
+      '/iframe.html?id=components-<%= name.toLowerCase() %>--default-story'
+    )
   })
 
   breakpoints.describeEvery(({ expectSnapshot }) => {})
