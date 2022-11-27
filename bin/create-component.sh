064a1dfa91b7665329477f81b160ca06f14fe479
anton202 <a.kluge202@gmail.com>
2022-11-27 15:52:24 +0200

    add bash script to create a component with only one argument && changed commands names

---
+++ bin/create-component.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# $1 = the first argument in the command line
+
+# trasforms component name from PascalCase to kebab-case
+componentNameInkebabCase=$(echo "$1" | sed 's/\(.\)\([A-Z]\)/\1-\2/g' | tr '[:upper:]' '[:lower:]')
+
+pnpm run create:component-scaffolding "$1" "$componentNameInkebabCase"
