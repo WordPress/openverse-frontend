040734773116bcb9e91a467e89feccaec99f8b50
Anton <a.kluge202@gmail.com>
2022-11-28 16:22:01 +0200

    Update bin/add-entry-to-tsconfig.js
    Co-authored-by: Olga Bulat <obulat@gmail.com>

--- bin/add-entry-to-tsconfig.js
+++ bin/add-entry-to-tsconfig.js
@@ -10,7 +10,7 @@ if (component_name === undefined)
 let tsconfig = parse(fs.readFileSync('./tsconfig.json', 'utf8'))
 
 tsconfig.include.push(
-  `src/components/${comoponent_name}/${comoponent_name}.vue`
+  `src/components/${component_name}/${component_name}.vue`
 )
 fs.writeFileSync('./tsconfig.json', stringify(tsconfig, null, 3), { flag: 'w' })
 
