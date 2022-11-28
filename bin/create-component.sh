5904019a43b6822a12da2d465e5b7fb571fa74cf
Anton <a.kluge202@gmail.com>
2022-11-28 16:20:57 +0200

    Update bin/create-component.sh
    Co-authored-by: Olga Bulat <obulat@gmail.com>

--- bin/create-component.sh
+++ bin/create-component.sh
@@ -5,4 +5,4 @@
 # trasforms component name from PascalCase to kebab-case
 componentNameInKebabCase=$(echo "$1" | sed 's/\(.\)\([A-Z]\)/\1-\2/g' | tr '[:upper:]' '[:lower:]')
 
-pnpm run create:component-scaffolding "$1" "$componentNameInkebabCase"
+pnpm run create:component-scaffolding "$1" "$componentNameInKebabCase"
