09b9bceafc6fbfa4242bc2227500b3b903b16148
Anton <a.kluge202@gmail.com>
2023-01-13 08:55:37 +0200

    Update bin/create-component.sh
    Co-authored-by: Zack Krida <zackkrida@pm.me>

--- bin/create-component.sh
+++ bin/create-component.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]
     echo "Please enter a component name"
     exit 1;
   else
-    transforms component name from PascalCase to kebab-case
+    # transforms component name from PascalCase to kebab-case
     componentNameInKebabCase=$(echo "$1" | sed 's/\(.\)\([A-Z]\)/\1-\2/g' | tr '[:upper:]' '[:lower:]')
 
     pnpm run create:component-scaffolding "$1" "$componentNameInKebabCase"
