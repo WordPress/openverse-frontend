521825bd30984dff56384470adcf9475be839a83
Zack Krida <zackkrida@pm.me>
2023-02-08 12:28:40 -0500

    Update src/server-middleware/robots.js

--- src/server-middleware/robots.js
+++ src/server-middleware/robots.js
@@ -1,13 +1,9 @@
 const { LOCAL, PRODUCTION } = require("../constants/deploy-env")
 
 /**
- * A simple healthcheck that is always true.
- *
- * @todo Update to a resource-sensitive version that fails when
- * memory and/or cpu usage reach a configurable threshhold.
- * @type {import('@nuxt/types').ServerMiddleware}
+ * Send the correct robots.txt information per-environment.
  */
-export default function healthcheck(_, res) {
+export default function robots(_, res) {
   const deployEnv = process.env.DEPLOYMENT_ENV ?? LOCAL
 
   const contents =
