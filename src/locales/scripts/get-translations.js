101f7b5ac8beef316762ff74efe9096f711749ba
Dhruv Bhanushali <dhruv_b@live.com>
2023-02-15 14:02:56 +0400

    Add bulk downloading functionality

--- src/locales/scripts/get-translations.js
+++ src/locales/scripts/get-translations.js
@@ -3,10 +3,12 @@
  * convert to our JSON format, and save in the correct folder.
  */
 const { writeFile } = require("fs/promises")
-const { writeFileSync } = require("fs")
+const { writeFileSync, createWriteStream } = require("fs")
 const os = require("os")
 
 const chokidar = require("chokidar")
+const qs = require("qs")
+const AdmZip = require("adm-zip")
 
 const axios = require("./axios")
 
@@ -24,7 +26,9 @@ const { parseJson } = require("./read-i18n")
  * @typedef {("android"|"po"|"mo"|"resx"|"strings"|"properties"|"json"|"jed1x"|"ngx" & JSONFormat)} Format
  */
 
-const baseUrl = `https://translate.wordpress.org/projects/meta/openverse`
+const baseUrl = "https://translate.wordpress.org/projects/meta/openverse"
+const bulkBaseUrl = "https://translate.wordpress.org/exporter/meta/openverse"
+const loginUrl = "https://login.wordpress.org/wp-login.php"
 
 /**
  *
@@ -121,6 +125,99 @@ const fetchAndConvertJed1xTranslations = (locales) => {
 }
 
 /**
+ * Get the URL to download a ZIP file containing all translation strings for all
+ * locales in the specified format.
+ *
+ * @param {Format} format - the
+ * @return {string}
+ */
+const makeBulkTranslationUrl = (format = "po") =>
+  `${bulkBaseUrl}/-do/?export-format=${format}`
+
+/**
+ * Fetch the ZIP of translations strings from GlotPress using the authentication
+ * cookies to access the page.
+ *
+ * @param cookies {string[]} - the cookies to authenticate the ZIP download
+ * @return {Promise<string>}} - the path to the downloaded ZIP file
+ */
+const fetchBulkJed1xTranslations = (cookies) =>
+  axios
+    .get(makeBulkTranslationUrl("jed1x"), {
+      headers: { cookie: cookies.join("; ") },
+      responseType: "stream",
+    })
+    .then((res) => {
+      return new Promise((resolve, reject) => {
+        const dest = process.cwd() + "/src/locales/openverse.zip"
+        const writer = createWriteStream(dest)
+        res.data.pipe(writer)
+        writer.on("error", reject)
+        writer.on("close", () => {
+          resolve(dest)
+        })
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+/**
+ * Given a username and password, login to WordPress and get the authentication
+ * cookies from the `Set-Cookie` header.
+ *
+ * @param log {string} - the username to log in with
+ * @param pwd {string} - the password for the given username
+ * @return {Promise<string[]>} - the list of cookies in the `Set-Cookie` header
+ */
+const getAuthCookies = (log, pwd) =>
+  axios
+    .post(
+      loginUrl,
+      qs.stringify({
+        log,
+        pwd,
+        rememberme: "forever",
+        "wp-submit": "Log In",
+        redirect_to: "https://make.wordpress.org/",
+      }),
+      {
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+        maxRedirects: 0,
+        validateStatus: (status) => status === 302, // successful login results in redirect
+      }
+    )
+    .then((res) =>
+      res.headers["set-cookie"].map((cookie) =>
+        cookie.substring(0, cookie.indexOf(";"))
+      )
+    )
+
+/**
+ * Extract all JSON file from the given ZIP file. Their names are sanitised to
+ * be in the format `<locale_code>.json`.
+ *
+ * @param zipPath {string} - the path to the ZIP file to extract
+ * @return {Promise<number>} - the number of locales successfully downloaded
+ */
+const extractZip = (zipPath) => {
+  const zip = new AdmZip(zipPath)
+  return Promise.all(
+    zip
+      .getEntries()
+      .filter((entry) => entry.entryName.endsWith(".json"))
+      .map((entry) => {
+        const jed1x = JSON.parse(zip.readAsText(entry))
+        const locale = entry.name
+          .replace("meta-openverse-", "")
+          .replace(".jed.json", "")
+        return [locale, jed1xJsonToJson(jed1x)]
+      })
+      .map((args) => writeLocaleFile(...args))
+  )
+}
+
+/**
  * Write `en.json` from `en.json5`.
  */
 const writeEnglish = () => {
@@ -144,11 +241,28 @@ if (process.argv.includes("--watch")) {
 }
 
 if (!process.argv.includes("--en-only")) {
-  const localeJSON = require("./wp-locales.json")
+  const username = process.env.GLOTPRESS_USERNAME
+  const password = process.env.GLOTPRESS_PASSWORD
+  if (username && password) {
+    console.log("Auth credentials found, performing bulk download.")
 
-  fetchAndConvertJed1xTranslations(Object.values(localeJSON).map((i) => i.slug))
-    .then((res) => {
-      console.log(`Successfully saved ${res.length + 1} translations.`)
-    })
-    .catch(console.error)
+    getAuthCookies(username, password)
+      .then(fetchBulkJed1xTranslations)
+      .then(extractZip)
+      .then((res) => {
+        console.log(`Successfully saved ${res.length + 1} translations.`)
+      })
+      .catch(console.error)
+  } else {
+    console.log("Auth credentials not found, performing parallel download.")
+
+    const localeJSON = require("./wp-locales.json")
+    fetchAndConvertJed1xTranslations(
+      Object.values(localeJSON).map((i) => i.slug)
+    )
+      .then((res) => {
+        console.log(`Successfully saved ${res.length + 1} translations.`)
+      })
+      .catch(console.error)
+  }
 }
